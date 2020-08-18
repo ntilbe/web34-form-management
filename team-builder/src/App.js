fdc803abe0b6b04260e4e02de1139ca82a5d86e2
Nicole Tilbe <nicolecali2406@outlook.com>
2020-08-18 16:51:00 -0400

    finished MVP and did some styling for stretch

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid'; 
 
-function App() {
+import './App.css';
+import './Components/background.css'
+import TeamMember from './Components/TeamMember';
+import Form from './Components/Form'
+
+const initialTeamList = [
+  {
+    id: uuid(), 
+    name: 'Nicole', 
+    email: 'nicole-tilbe@lambdastudents.com', 
+    role: 'Full Stack Developer',
+  }
+]
+
+const initialFormValues = {
+  // text inputs
+  name: '', 
+  email: '',
+  // dropdown options
+  role: '',
+}
+
+export default function App() {
+  // giving state variables initial value
+  const [teamMembers, setTeamMembers] = useState(initialTeamList)
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const onInputChange = e => {
+    const { name } = e.target
+    const { value } = e.target
+    setFormValues({...formValues, [name]: value})
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    if (
+      !formValues.name.trim() ||
+      !formValues.email.trim() ||
+      !formValues.role.trim())
+    {
+      return 
+    }
+    const newMember = { ...formValues, id: uuid() }
+
+    setTeamMembers([newMember, ...teamMembers])
+
+    setFormValues(initialFormValues)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App container"> 
+      <header>
+        <h1>Team Builder App</h1>
       </header>
+
+      <Form
+        values={formValues}
+        onInputChange={onInputChange}
+        onSubmit={onSubmit}
+      />
+
+      {/* mapping through teamMembers array and 'consuming' TeamMember component for each */}
+      <div className="teamMembers">
+        {
+          teamMembers.map(member => {
+            return (
+              <TeamMember key={member.id} details={member} />
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
-
-export default App;
